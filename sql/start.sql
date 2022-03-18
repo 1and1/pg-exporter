79adac4a0c4265bc9cb4cea59f20361bc39ff69c
Christoph Berg <myon@debian.org>
2022-03-18 15:19:03 +0100

    Add Testsuite based on PGXS
    Close #1.

---
+++ sql/start.sql
@@ -0,0 +1,10 @@
+show port \gset
+\setenv PGPORT :port
+show unix_socket_directories \gset
+\setenv PGHOST :unix_socket_directories
+select current_database() as database \gset
+\setenv PGDATABASE :database
+
+\! ./pg-exporter --log.level=warn & echo $! > pid; sleep 0.1
+
+\! curl -sf localhost:9135/metrics | grep postgresql_up
