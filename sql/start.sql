0d0bda818a5b2d4ee4306d5053831c601a664c42
Thomas Berger <thomas.berger@1und1.de>
2022-06-15 14:30:30 +0200

    completed regression tests

--- sql/start.sql
+++ sql/start.sql
@@ -5,6 +5,6 @@ show unix_socket_directories \gset
 select current_database() as database \gset
 \setenv PGDATABASE :database
 
-\! ./pg-exporter --log.level=warn & echo $! > pid; sleep 0.1
+\! ./bin/pg_exporter --log.level=warn & echo $! > pid; sleep 0.1
 
 \! curl -sf localhost:9135/metrics | grep postgresql_up
