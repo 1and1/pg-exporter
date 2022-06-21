dd91fc14655244aa77a2d98ce9d70c427701bccd
Christoph Berg <myon@debian.org>
2022-06-21 12:03:54 +0200

    Drop postgresql_settings_server_version_num help from tests
    The min/max values include the version number.

--- sql/all.sql
+++ sql/all.sql
@@ -5,4 +5,5 @@ select current_setting('server_version_num')::int / 10000 as pg_major_version;
 -- remove Go and PostgreSQL version numbers
 -- remove postgresql_locks_count{mode="RowExclusiveLock"} line which sometimes pops up
 -- remove debug_discard_caches help since its max value depends on --enable-cassert
-\! curl -sf localhost:9135/metrics | sed -e 's/^\([a-z].*\) .*/\1/' -e 's/database_id="[0-9]*"/database_id=""/' -e 's/version="[^"]*"/version=""/' -e '/mode="RowExclusiveLock"/d' -e '/# HELP postgresql_settings_debug_discard_caches/d'
+-- remove server_version_num help since its min/max values depends on the PG version
+\! curl -sf localhost:9135/metrics | sed -e 's/^\([a-z].*\) .*/\1/' -e 's/database_id="[0-9]*"/database_id=""/' -e 's/version="[^"]*"/version=""/' -e '/mode="RowExclusiveLock"/d' -e '/# HELP postgresql_settings_debug_discard_caches/d' -e '/# HELP postgresql_settings_server_version_num/d'
